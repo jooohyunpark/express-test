a8be30146b3381a7607ceb36609267dd5e252012
jooohyunpark <jhp527@nyu.edu>
2022-01-11 15:19:55 -0500

    organize app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var http = require("http");
+const http = require("http");
 
-http
-  .createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { "Content-Type": "text/plain" });
+const hostname = "127.0.0.1";
+const port = 8000;
 
-    // Send the response body as "Hello World"
-    response.end("Hello World\n");
-  })
-  .listen(8081);
+const server = http.createServer(function (request, response) {
+  // Send the HTTP header
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, { "Content-Type": "text/plain" });
 
-// Console will print the message
-console.log("Server running at http://127.0.0.1:8081/");
+  // Send the response body as "Hello World"
+  response.end("Hello World\n");
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
