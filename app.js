4a91c02fcffa309923531a3aa68754d00eef4cab
jooohyunpark <jhp527@nyu.edu>
2022-01-12 20:21:47 -0500

    test page route

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+  res.sendFile(`${__dirname}/src/index.html`);
+});
+
+app.get("/page", (req, res) => {
+  res.sendFile(`${__dirname}/src/page/index.html`);
 });
 
 app.listen(port, () => {
