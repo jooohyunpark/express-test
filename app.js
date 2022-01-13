69be686e9e4db8cfe9821c6ed26ed4a543c5a757
jooohyunpark <jhp527@nyu.edu>
2022-01-12 20:16:56 -0500

    link  assets in html

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(port, () => {
